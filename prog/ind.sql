4be1c9094a3b99e39034a46e8bc9747b7e686408
FiaLDI <leon.youtube2017@yandex.ru>
2024-04-23 14:24:44 +0300

    data

--- prog/ind.sql
+++ prog/ind.sql
@@ -1,9 +1,9 @@
 .import --csv housing.csv house
 
 .mode json
-
+SELECT * FROM house;
 SELECT * FROM house limit 4;
-.shcema house;
+
 SELECT count(total_rooms) FROM house WHERE total_rooms < 1000;
 SELECT count(total_rooms) FROM house WHERE total_rooms > 1000;
 SELECT * FROM house WHERE total_rooms < 1000;
